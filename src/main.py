96b9d3fc57e21b5978efd8dc2201d668493e45cb
sarahaj32 <sarahj32@berkeley.edu>
2024-10-21 14:34:25 -0700

    added a check, fixed comments

--- src/main.py
+++ src/main.py
@@ -14,118 +14,125 @@ def main():
 
     # Pseudohaploid
     ph_subparser = subparser.add_parser('pseudohaploid', help='convert diploid data to pseudohaploid')
-    ph_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
+    ph_subparser.add_argument("-vcf",help="[Required] path to vcf to simulate damage in", type=str, required = True)
     ph_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf")
     ph_subparser.add_argument("-targets", metavar='',help="Target individuals to simulate features on", type=str, default = "")
 
     # Deamination
     deamination_subparser = subparser.add_parser('deaminate', help='Simulate deamination at given rate')
-    deamination_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
+    deamination_subparser.add_argument("-vcf",help="[Required] path to vcf to simulate damage in", type=str, required = True)
     deamination_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf")
     deamination_subparser.add_argument("-targets", metavar='',help="Target individuals to simulate features on", type=str, default = "")
     deamination_subparser.add_argument("-rate", metavar='',help="rate at which to convert to deminated", type=float, default=0.05)
 
     # Contamination
     contam_subparser = subparser.add_parser('contaminate', help='Simulate contamination (either ancestral or modern human)')
-    contam_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
+    contam_subparser.add_argument("-vcf",help="[Required] path to vcf to simulate damage in", type=str, required = True)
     contam_subparser.add_argument("-mh",help="modern human flag", action = 'store_true')
-    contam_subparser.add_argument("-anc",help="ancestral flag human flag", action = 'store_true')
+    contam_subparser.add_argument("-ancestral",help="ancestral flag human flag", action = 'store_true')
     contam_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf") 
     contam_subparser.add_argument("-targets", metavar='',help="Target individuals to simulate features on", type=str, default = "")
-    contam_subparser.add_argument("-rate", metavar='',help="rate at which to convert to deminated", type=float, default=0.05)
+    contam_subparser.add_argument("-rate", metavar='',help="rate at which to contaminate sites of target individuals", type=float, default=0.05)
     contam_subparser.add_argument("-length", metavar='',help="length of contaminating modern human fragments", type=int, default=1000)
-    contam_subparser.add_argument("-modern", metavar='',help="Modern human individuals to simulate features on", type=str, default = "")
+    contam_subparser.add_argument("-modern", metavar='',help="Modern human individuals to contaminate with", type=str, default = "")
 
     # downsample VCF
     downsample_subparser = subparser.add_parser('downsample', help='Downsample VCF')
-    downsample_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
+    downsample_subparser.add_argument("-vcf",help="[Required] path to vcf to simulate damage in", type=str, required = True)
     downsample_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf")
     downsample_subparser.add_argument("-num", metavar='',help="number of snps to downsample to", type=int, default=30_000)
 
     # add missingness
     missing_subparser = subparser.add_parser('missing', help='add missingness to VCF')
-    missing_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
+    missing_subparser.add_argument("-vcf",help="[Required] path to vcf to simulate damage in", type=str, required = True)
     missing_subparser.add_argument("-targets", metavar='',help="Target individuals to simulate features on", type=str, default = "")
     missing_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf")
-    missing_subparser.add_argument("-rate", metavar='',help="rate of missingness to induce", type=float, default=.1)
+    missing_subparser.add_argument("-rate", metavar='',help="rate of missingness to simulate", type=float, default=0.1)
 
     # add depth and filter
     depth_subparser = subparser.add_parser('dpFilter', help='Downsample VCF')
     depth_subparser.add_argument("-vcf",help="path to vcf to simulate damage in", type=str, required = True)
     depth_subparser.add_argument("-out",help="path to output simulated vcf", type=str, default = "out.vcf")
     depth_subparser.add_argument("-mean", metavar='',help="mean depth to simulate", type=int, default=5)
-    depth_subparser.add_argument("-r", "--variance", metavar='',help="variance of depth to simulate", type=int, default=2)
+    depth_subparser.add_argument("-variance", metavar='',help="variance of depth to simulate", type=int, default=2)
     # allowing varying disbributions has not been implemented yet
     # depth_subparser.add_argument("-distribution", metavar='',help="distribution to use to simulate depth from", type=str, default="normal")
     depth_subparser.add_argument("-missing", help="flag that indiciates to remove sites with 0 reads", action = "store_true")
     depth_subparser.add_argument("-annotate", help="flag that indiciates to only annotate with depth", action = "store_true")
     depth_subparser.add_argument("-targets", metavar='',help="Target individuals to simulate features on", type=str, default = "")
 
-    # futuer: could make target and sources files? 
+    # future: could make target and sources files? 
     args = parser.parse_args()
 
-    # run the features (for now, only one can be called at a time)
-    if args.mode == "pseudohaploid":
-        sample_list = parse_target_indivs(args.targets)
-        print("Pseudohaplotyping")
-        make_pseudohaploid(args.vcf, args.out, sample_list)
-        print("finished pseudohaploid calls")
-    
-    if args.mode == "contaminate":
-        sample_list = parse_target_indivs(args.targets)
-        if args.anc:
-            print(f"Adding ancestral contamination at {args.rate} rate")
-            add_anc_contamination(args.vcf, args.out, sample_list, args.rate)
-            print("Finished adding ancestral contamination")
-        elif args.mh:
-            if args.modern == "":
-                raise Exception("modern human population is necessary for adding modern human contamination")
-            print(f"Adding modern contamination from {args.modern} at {args.rate} rate")
-            add_mh_contamination(args.vcf, args.out, sample_list, args.modern, args.rate, args.length)
-            print("Finished adding modern contamination")
-        else:
-            raise Exception("must specify either anc for ancestral contamination or mh for modern human contamination")
+    # check if a simulation mode was provided
+    if not args.mode:
+        print("\nA simulation mode is required. Options are:")
+        print("pseudohaploid, deaminate, contaminate, downsample, missing, dpFilter\n")
 
-    if args.mode == "deaminate":
-        sample_list = parse_target_indivs(args.targets)
-        if not (0 < args.rate < 1):
-            raise Exception("deamination rate must be above 0 and less than 1")
-        else:
-            print("Adding deamination")
-            add_deam(args.vcf, args.out, sample_list, args.rate)
-            print("Finished adding deamination")
+    else:
+        # run the features (for now, only one can be called at a time)
+        if args.mode == "pseudohaploid":
+            sample_list = parse_target_indivs(args.targets)
+            print("Pseudohaplotyping")
+            make_pseudohaploid(args.vcf, args.out, sample_list)
+            print("finished pseudohaploid calls")
+        
+        if args.mode == "contaminate":
+            sample_list = parse_target_indivs(args.targets)
+            if args.ancestral:
+                print(f"Adding ancestral contamination at {args.rate} rate")
+                add_anc_contamination(args.vcf, args.out, sample_list, args.rate)
+                print("Finished adding ancestral contamination")
+            elif args.mh:
+                if args.modern == "":
+                    raise Exception("modern human population is necessary for adding modern human contamination")
+                print(f"Adding modern contamination from {args.modern} at {args.rate} rate")
+                modern_list = parse_target_indivs(args.modern)
+                add_mh_contamination(args.vcf, args.out, sample_list, modern_list, args.rate, args.length)
+                print("Finished adding modern contamination")
+            else:
+                raise Exception("must specify either anc for ancestral contamination or mh for modern human contamination")
 
-    if args.mode == "dpFilter":
-        sample_list = parse_target_indivs(args.targets)
-        if args.missing:
-            print("Adding depth abbitations, including missingness, and creating false homozygotes")
-            fun = "pos_depth_all"
-        elif args.annotate:
-            print("Adding depth annotations")
-            fun = "pos_depth_only"
-        else: # by default - annotate and add false homozygotes but don't induce missingness
-            print("Adding depth abbitations and creating false homozygotes")
-            fun = "pos_depth_homo"
-    
-        add_depth(args.vcf, args.out, sample_list, args.mean, args.variance, fun) # args.distribution, 
-        print("Finished adding depth")
+        if args.mode == "deaminate":
+            sample_list = parse_target_indivs(args.targets)
+            if not (0 < args.rate < 1):
+                raise Exception("deamination rate must be above 0 and less than 1")
+            else:
+                print("Adding deamination")
+                add_deam(args.vcf, args.out, sample_list, args.rate)
+                print("Finished adding deamination")
 
-    if args.mode == "downsample":
-        if args.num <=0:
-            raise Exception("num of positions must be > 0")
-        else:
-            print(f"Beginnning downsampling to {args.num} positions")
-            downsample(args.vcf, args.out, args.num)
-            print("Finished downsampling")
+        if args.mode == "dpFilter":
+            sample_list = parse_target_indivs(args.targets)
+            if args.missing:
+                print("Adding depth abbitations, including missingness, and creating false homozygotes")
+                fun = "pos_depth_all"
+            elif args.annotate:
+                print("Adding depth annotations")
+                fun = "pos_depth_only"
+            else: # by default - annotate and add false homozygotes but don't induce missingness
+                print("Adding depth abbitations and creating false homozygotes")
+                fun = "pos_depth_homo"
+        
+            add_depth(args.vcf, args.out, sample_list, args.mean, args.variance, fun) # args.distribution, 
+            print("Finished adding depth")
 
-    if args.mode == "missing":
-        sample_list = parse_target_indivs(args.targets)
-        if not (0 < args.rate < 1):
-            raise Exception("rate must be between 0 and 1")
-        else:
-            print(f"Adding missingness at rate: {args.rate}")
-            add_missingness(args.vcf, args.out, sample_list, args.rate)
-            print("Finished adding missingness")
+        if args.mode == "downsample":
+            if args.num <=0:
+                raise Exception("num of positions must be > 0")
+            else:
+                print(f"Beginnning downsampling to {args.num} positions")
+                downsample(args.vcf, args.out, args.num)
+                print("Finished downsampling")
+
+        if args.mode == "missing":
+            sample_list = parse_target_indivs(args.targets)
+            if not (0 < args.rate < 1):
+                raise Exception("rate must be between 0 and 1")
+            else:
+                print(f"Adding missingness at rate: {args.rate}")
+                add_missingness(args.vcf, args.out, sample_list, args.rate)
+                print("Finished adding missingness")
 
 if __name__ == "__main__":
     print("starting")
