cb8d7deb63926ef894f7ae0ce17c2e7731bac7da
sarahaj32 <sarahj32@berkeley.edu>
2024-10-21 11:17:43 -0700

    updated comments, allow non-biallelic genotypes

--- src/pseudohaploid_sim.py
+++ src/pseudohaploid_sim.py
@@ -1,29 +1,38 @@
+"""
+Helper file with functions to randomly sample an allele
+from heterozygous positions, outputting pseudo-haploid genotype calls
+"""
 from helper_functions import parse_header
 import random
 
 def ph_geno_call(base):
+    """
+    helper function that randomly selects an allele
+    from the given genotypes
+    """
     if base[0] == base[2]:
         return base
     else:
-        call = round(random.random())
+        # randomly select one of the two alleles, and make homozygous for that allele
+        call = random.choice([base[0], base[2]])
         return(str(call) + base[1] + str(call) + base[3:])
 
 def make_pseudohaploid(vcf_path, new_vcf, sample_list):
+    """
+    Finds the individuals to be pseudohaplotyped,
+    at heterozygous sites, randomly selects an allele
+    and converts genotype to homozygous for that allele
+    """
     with open(vcf_path, "r") as file1, open(new_vcf, "w") as outfile:
         for line in file1:
             line = line.strip().split("\t")
-            if line[0].startswith("#"):  # looking at if every time
+            if line[0].startswith("#"): 
                 outfile.write("\t".join(line) + "\n")
                 if "#CHROM" in line[0]:
                     # include: the indices of the samples for the simulation to be applied to
                     _, include = parse_header(line, sample_list)
-                    # if include == []:
-                    #     print("provided population names not in file. Breaking")
-                    #     break
-                    # else:
-                        # update this to say all samples if no sample_list is provided
                     print(f"making {sample_list} samples pseudohaploid")
-                    #print(f"include:{include}")
             else:
+                # convert to pseudohaploid if the individual is specified to be pseudohaploid
                 line = [ph_geno_call(line[i]) if i in include else line[i] for i in range(len(line))]
                 outfile.write("\t".join(line) + "\n")
